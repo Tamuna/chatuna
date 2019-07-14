772ef98ae83db22be841ccdf474e5b74b2b88f64
tamuna <tgeld15@freeuni.edu.ge>
2019-07-14 22:18:44 +0400

    Initial commit

---
+++ app/src/main/java/ge/edu/freeuni/chatuna/MainActivity.java
@@ -0,0 +1,14 @@
+package ge.edu.freeuni.chatuna;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
