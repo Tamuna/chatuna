805ed2f6dee0221fb26da021ac514c5e5a2366a2
ekavt15 <ekavt15@freeuni.edu.ge>
2019-07-25 17:53:13 +0400

    Add entity classes

---
+++ app/src/main/java/ge/edu/freeuni/chatuna/data/Message.java
@@ -0,0 +1,70 @@
+package ge.edu.freeuni.chatuna.data;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity(tableName = "messages")
+public class Message {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "message_text")
+    private String messageText;
+
+    @ColumnInfo(name = "create_date")
+    private Date createDate;
+
+    @ColumnInfo(name = "user_id")
+    private int userId;
+
+    @Ignore
+    public Message(String messageText, Date createDate, int userId) {
+        setId(-1);
+        setMessageText(messageText);
+        setCreateDate(createDate);
+        setUserId(userId);
+    }
+
+    public Message(int id, String messageText, Date createDate, int userId) {
+        setId(id);
+        setMessageText(messageText);
+        setCreateDate(createDate);
+        setUserId(userId);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String text) {
+        this.messageText = text;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
---
+++ app/src/main/java/ge/edu/freeuni/chatuna/data/User.java
@@ -0,0 +1,45 @@
+package ge.edu.freeuni.chatuna.data;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "users")
+public class User {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @ColumnInfo(name = "name")
+    private String name;
+
+    @Ignore
+    public User(String name){
+        setId(-1);
+        setName(name);
+    }
+
+    public User(int id, String name) {
+        setId(id);
+        setName(name);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+}
