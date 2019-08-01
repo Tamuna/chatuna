014accdd80bf48a8ab993a8a19c63ac96712c48d
tamuna <tgeld15@freeuni.edu.ge>
2019-08-01 18:45:48 +0400

    add peers list on chat view

--- app/src/main/java/ge/edu/freeuni/chatuna/main/WiFiP2pBroadcastReceiver.java
+++ app/src/main/java/ge/edu/freeuni/chatuna/main/WiFiP2pBroadcastReceiver.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.util.Log;
 
